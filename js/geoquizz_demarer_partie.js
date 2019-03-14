746106e38f6554fe269297c3e1c240e23d16706d
Leo Galassi <leo.galassi1@etu.univ-lorraine.fr>
2019-03-14 13:36:50 +0100

    Passade de la navBar dans un unique component

--- js/geoquizz_demarer_partie.js
+++ js/geoquizz_demarer_partie.js
@@ -1,28 +1,68 @@
-Vue.component('serie',{
+Vue.component('navbar',{
+  data: function () {
+    return {
+      serieFilter:'',
+      niveauFilter:'',
+      pseudo : '',
+    }
+  },
+  methods: {
+    getSerie(event){
+      this.serieFilter = event.target.value;
+      console.log(this.serieFilter);
+    },
+    getNiveau(event){
+      this.niveauFilter = event.target.value;
+      console.log(this.niveauFilter);
+    },
+  },
   template: `
+  <nav class="navbar navbar-light bg-light d-flex flex-row">   
+              <div class="input-group">
+              <img src="images/logo.png"  style="width: 3%; height: 3%" >
+              <div class="input-group-prepend mr-2">
 
-  <select class="selectpicker ml-2 mr-2 bg-secondary text-white" @change="onChangeSerie($event)">
-                            <option v-for="serie in liste_serie" selected value="">{{serie.ville}}</option> 
+            <select class="selectpicker ml-2 mr-2 bg-secondary text-white" @change="getSerie($event)" v-model="serieFilter">
+                            <option value="" selected disabled hidden>Série</option>
+                            <option v-for="serie in liste_serie" selected :value="serie.ville">{{serie.ville}}</option> 
                          </select>
+
+           <select class="selectpicker ml-2 mr-2 bg-secondary text-white" @change="getNiveau($event)">
+           <option value="" selected disabled hidden>Niveau</option>           
+           <option value="normal">Normal</option>                      
+           </select>
+                 </div>
+
+          <input type="text" class="form-control text-center" placeholder="Votre pseudo..." aria-label="pseudo" v-model="pseudo" aria-describedby="button-addon2" v-on:keyup.enter="getSeries()"/>
+          <div class="input-group-append w-25">
+
+          <button v-on:click="getSeries()" class="btn btn-secondary ml-2">
+          Lancer la partie
+         </button>
+          </div>
+          
+          </div>
+          </nav> 
+            
    `,
   props : ['liste_serie'],
 
 });
 
 
+//----------------------------------------INSTANCE DE VUE---------------------------------------
+
 var content = new Vue({
     el: '#main_page',
-    data: {
-      serie : '',
-      difficulte : '',
-      pseudo : '',
+    data: {     
       listeSeries:'',
     },
     methods: {
+    
       
     },
     computed: {
-  
+     
     },
     created() {
       
