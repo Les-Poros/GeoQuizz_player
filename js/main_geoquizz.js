dcb383d5f1dde50d61339fa7d06e3dc4c92cf79f
Leo Galassi <leo.galassi1@etu.univ-lorraine.fr>
2019-03-15 20:31:57 +0100

    on récupère les photos de la série et on les affiches toutes pour l'instant

--- js/main_geoquizz.js
+++ js/main_geoquizz.js
@@ -81,6 +81,7 @@ Vue.component('start',{
     });
     },
     template: `
+    
     <nav class="navbar navbar-light bg-light d-flex flex-row">   
                 <div class="input-group">
                 <img src="images/logo.png"  style="width: 3%; height: 3%" >
@@ -107,55 +108,73 @@ Vue.component('start',{
             
             </div>
             </nav> 
-              
-     `,
+             `,
     props : ['liste_serie'],
   
   });
   
   
   Vue.component('game',{
+    data: function () {
+        return {
+            cible : '',
+            pos:'',
+            lat: '',
+            lon:'',
+            clique:'',
+            res:'',
+            zone:'',
 
+        }
+      },
+    created(){
+        // this.mymap = L.map('mapid').setView([48.6843900,  6.1849600], 13);
+            
+        //     this.tileStreets = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        //         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        //         maxZoom : 18,
+        //         minZoom : 12,
+        //         id: 'mapbox.streets',
+        //         accessToken: 'pk.eyJ1IjoibGVvbGV6aWciLCJhIjoiY2p0NGk5bGhxMDN6MjN5bnc0dWo5M2w1YSJ9.Rp94LWSF0ljKG8zCV2MdBw'
+                
+        //         });
+               
+        //     this.tileStreets.addTo(this.mymap);
+        //     this.cible = L.marker([48.6939,6.182909999999993]).addTo(this.mymap);
+        //     this.zone = L.circle([48.6843900,  6.1849600], {radius: 2000}).addTo(this.mymap);
+    },
     template: `
+    <div>
     <nav class="navbar navbar-light bg-light d-flex flex-row nav-text">   
                 <div class="input-group text-center">
                 <img src="images/logo.png"  style="width: 3%; height: 3%" >
                 <h2> Serie : Nancy - Photo(1/10)</h2>
                 </div>
             </nav> 
+    <div class="row">
+    <div class="col-lg-7">
               
+    <div id="mapid" style="height: 500px">
+    </div>
+
+    </div>
+
+    <div class="col-lg-5" >
+    <div v-for="img in liste_photo">
+        
+        <img :src="img.url" style="width:100%; height :100%;margin-right: 10px;" >
+    </div>
+    </div>
+
+    </div>
+    </div>
+    
      `,
+     props : ['liste_photo'],
    
   
   });
     
-
-
-
-Vue.component('zone_jeu',{
-    data: function () {
-      return {
-       
-      }
-    },
-    methods: {
-      
-    },
-    template: `
-    
-    <div class="row">
-    <div class="col-sm-5">
-      
-      <img src="images/stan.jpg" style="width : 100%; height :100%" >
-    </div>
-    <div class="col-sm-7">
-      zone carte 
-    </div>
-    </div>
-     `,
-    props : [],
-  
-  });
 
 
   var content = new Vue({
@@ -164,14 +183,33 @@ Vue.component('zone_jeu',{
         isAccueil : true,
         idPartie:'',
         token:'',
-       
+        listePhoto: [],
          
    },
     methods: {
        startGame(info){
            this.idPartie =info[0];
            this.token = info[1];
-       }
+            console.log('token : '+this.token + 'idPartie : '+ this.idPartie);
+       
+            axios.get('http://192.168.99.100:8082/parties/'+this.idPartie,{
+                headers: {
+                    'x-lbs-token': this.token,
+                    
+                },
+        
+            })           
+            .then(response => {
+                //On vérifie qu'il y a bien un résultat
+                if(response.data.photo.length > 0){
+                  this.listePhoto = response.data.photo;
+
+                  console.log(this.listePhoto);
+                    
+                }
+            });
+        
+    },
     },
     computed: {
      
