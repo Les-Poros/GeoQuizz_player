01f1e63d9420290a698a73b3b95b301fd6d6f186
Leo Galassi <leo.galassi1@etu.univ-lorraine.fr>
2019-03-15 10:17:16 +0100

    Post non fonctionnel pour l'instant pour créer la partie

--- js/geoquizz_demarer_partie.js
+++ js/geoquizz_demarer_partie.js
@@ -1,20 +1,46 @@
 Vue.component('navbar',{
   data: function () {
     return {
-      serieFilter:'',
+     
       niveauFilter:'',
       pseudo : '',
+      idSerie:'',
     }
   },
   methods: {
     getSerie(event){
-      this.serieFilter = event.target.value;
-      console.log(this.serieFilter);
+      
+      this.idSerie = event.target.value;
+      console.log(this.idSerie);
     },
     getNiveau(event){
       this.niveauFilter = event.target.value;
       console.log(this.niveauFilter);
     },
+
+    sendData(){
+      if(this.idSerie && this.niveauFilter && this.pseudo != null){
+        this.postBody = {
+          "id": this.idSerie,
+          "joueur": this.pseudo,
+        };
+              
+        axios
+        .post('http://192.168.99.100:8082/parties/series/'+this.idSerie, this.postBody, {
+          headers: {
+            "Content-Type": "application/json"
+          },
+        })
+        .then(response => {
+         console.log('succes');
+        })
+        .catch(error => {
+          console.log(error);
+        });
+      
+      }
+    },
+     
   },
   template: `
   <nav class="navbar navbar-light bg-light d-flex flex-row">   
@@ -22,9 +48,9 @@ Vue.component('navbar',{
               <img src="images/logo.png"  style="width: 3%; height: 3%" >
               <div class="input-group-prepend mr-2">
 
-            <select class="selectpicker ml-2 mr-2 bg-secondary text-white" @change="getSerie($event)" v-model="serieFilter">
+            <select class="selectpicker ml-2 mr-2 bg-secondary text-white" @change="getSerie($event)" v-model="idSerie">
                             <option value="" selected disabled hidden>Série</option>
-                            <option v-for="serie in liste_serie" selected :value="serie.ville">{{serie.ville}}</option> 
+                            <option v-for="serie in liste_serie" selected :value="serie.id">{{serie.ville}}</option> 
                          </select>
 
            <select class="selectpicker ml-2 mr-2 bg-secondary text-white" @change="getNiveau($event)">
@@ -33,10 +59,10 @@ Vue.component('navbar',{
            </select>
                  </div>
 
-          <input type="text" class="form-control text-center" placeholder="Votre pseudo..." aria-label="pseudo" v-model="pseudo" aria-describedby="button-addon2" v-on:keyup.enter="getSeries()"/>
+          <input type="text" class="form-control text-center" placeholder="Votre pseudo..." aria-label="pseudo" v-model="pseudo" aria-describedby="button-addon2" v-on:keyup.enter="sendData()"/>
           <div class="input-group-append w-25">
 
-          <button v-on:click="getSeries()" class="btn btn-secondary ml-2">
+          <button v-on:click="sendData()" class="btn btn-secondary ml-2">
           Lancer la partie
          </button>
           </div>
@@ -58,15 +84,13 @@ var content = new Vue({
       listeSeries:'',
     },
     methods: {
-    
-      
+          
     },
     computed: {
      
     },
     created() {
       
-        
         axios.get('http://192.168.99.100:8083/series',{
         
         })
@@ -75,14 +99,12 @@ var content = new Vue({
             //On vérifie qu'il y a bien un résultat
             if(response.data.content.length > 0){
               this.listeSeries = response.data.content;
-              console.log(response.data.content[0].ville);
+              
              
             }
-
         
     });
-    
-  
+      
     }
   });
   
